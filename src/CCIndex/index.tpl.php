2140b5edc14afad20804bbf38f554de245fd35a7
DANIEL SPANDEL <dasp11@seekers.student.bth.se>
2012-05-10 14:03:48 +0200

    small update

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -8,7 +8,7 @@ Welcome to Bapelsin index controller
 <p>You can view the source directly at <a href="https://github.com/spandel/Bapelsin">https://github.com/spandel/Bapelsin</a></p>
 <h2>Step 2 - Make the data directory writable</h2>
 <p>Make the <code>site/data</code>-directory writable. This is the place where Bapelsin needs to be able to write and create files.</p>
-<blockquote><code>cd bapelsin; chmod 777 site/data</code></blockquote>
+<blockquote><code>cd Bapelsin; chmod 777 site/data</code></blockquote>
 <h2>Step 3 - Initialization</h2>
 <p>Some modules need to be initialized. Just click the following link!</p>
 <blockquote><a href="<?=create_url("modules/install")?>">modules/install</a></blockquote>
